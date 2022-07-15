b358c545cafb9c19a30c18b46d7a63de3e6467c2
Amanda Favaro <amandacfa@hotmail.com>
2022-07-15 16:44:10 -0300

    Initial commit

---
+++ Reflection/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DisplayPublicProperties(new Student("Amanda", "Let's Code", 25));
+            Console.WriteLine();
+            CreateInstance("Amanda", "Let's Code", 25);
+        }
+
+        public static void DisplayPublicProperties(Object obj)
+        {
+            foreach (var property in typeof(Student).GetProperties())
+            {
+                var pName = property.Name;
+                var pValue = property.GetValue(obj);
+                Console.WriteLine($"{pName}: {pValue}");
+            }
+        }
+
+        public static void CreateInstance(string pName, string pUniversity, int pRollNumber)
+        {
+            var assembly = Assembly.GetExecutingAssembly().GetTypes();
+
+            foreach (var property in assembly)
+            {
+                if (property.Name == "Student")
+                {
+                    var obj = (Student)Activator.CreateInstance(property);
+                    if (obj != null)
+                    {
+                        obj.Name = pName;
+                        obj.University = pUniversity;
+                        obj.RollNumber = pRollNumber;
+
+                        obj.DisplayInfo();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Reflection/Student.cs
@@ -0,0 +1,26 @@
+﻿namespace Reflection
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string University { get; set; }
+        public int RollNumber { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, string university, int rollNumber)
+        {
+            Name = name;
+            University = university;
+            RollNumber = rollNumber;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} - {University} - {RollNumber}");
+        }
+    }
+}
